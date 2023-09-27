d4266f46e0e32ba240bfec05b568fe229dd48f15
Felipe Seiberlich <philzaiber@outlook.com>
2023-09-27 22:25:49 +0000

    Add choices from 2 until 6 in the menu

--- run.py
+++ run.py
@@ -61,22 +61,74 @@ print('Select one of the following options bellow and be connected.\n')
 contact = {}
 
 def display_contact():
-    print("==========================================")
-    print("|Name            | Number           |")
-    print("==========================================")
+    print("=================================================")
+    print("|Name                        | Number           |")
+    print("=================================================")
     for key in contact:
         # format to left-align and give some spacing
         print("{}\t\t{}". format(key, contact.get(key)))
-    print("==========================================")
+    print("=================================================")
 
 while True:
     choice = int(input(" 1. Add new contact\n 2. Search contact\n 3. Display contact\n 4. Edit contact\n 5. Delete contact\n 6. Exit\n \nEnter your choice: "))
     if choice == 1:
-        print('Option 1 selected')
+        print('Option 1 selected!\n')
+        print('Please enter you name and surname.')
+        print('Example: Philip Grant')
+
         name = input("\nEnter the contact name: ")
+        print(' ')
+        
+        print('Please enter your phone number bellow only using numbers.')
+        print('Example: 00 353 892516666\n')
         phone = input("Enter the mobile number: ")
-        contact[name] = phone
+        print(' ')
+        print('Please double check your data...')
+        print(' ')
+
+        contact[name] = phone 
         display_contact()
+        print(' ')
+        print('What would you like to do now?')
+        print(' ')
+
+    elif choice == 2:
+        search_name = input("Enter the contact name: ")
+        if search_name in contact:
+            print(search_name, "'s contact number is", contact[search_name])
+        else:
+            print(' ')
+            print("Sorry :( This name is not present in this contact book.\n")
+            print('You can try a new search or select one of the other options bellow :)\n')
+
+    elif choice == 3:
+        if not contact:
+            print("Empty contact book")
+        else:
+            display_contact()
+            print(' ')
+    elif choice == 4:
+        edit_contact = input("Enter the contact to be edited: ")
+        if edit_contact in contact:
+            phone = input("Enter mobile number")
+            contact[edit_contact] = phone
+            print(' ')
+            print("contact_updated")
+            display_contact( )
+        else:
+            print("Name is not found in contact book")
+    elif choice == 5:
+        del_contact = input("Enter the contact to be deleted")
+        if del_contact in contact:
+            confirm = input("Do you want to delete this contact y/n?")
+            if confirm == 'y' or confirm == 'Y':
+                contact.pop(del_contact)
+            display_contact( )
+        else:
+            print("Name is not found in contact book")
+    else:
+        break
+    
 
 
 
